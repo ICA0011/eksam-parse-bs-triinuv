ea88d401ffd4a6db842e773935b2f1aa4121f7a0
Triinu-Liis Vaikma <92437926+triinuv@users.noreply.github.com>
2022-05-26 12:56:53 +0300

    Update parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
+
 def parse_xml():
-  url = "http://upload.itcollege.ee/~aleksei/test.xml"
-  xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
-  
-  # your code here
-  
-  return result
+    """Your script must return the content of <data> section from the file test.xml"""
+    url = "http://upload.itcollege.ee/~aleksei/test.xml"
+    xml_content = requests.get(url).content
+    soup = BeautifulSoup(xml_content, 'xml')
+    titles = soup.find_all("data")
+    
+    for title in titles:
+        word = title.get_text()
+        return str(word.strip())
